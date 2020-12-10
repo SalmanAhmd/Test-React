929fe464595fce5329d35cafb34853a17aaebe40
SalmanAhmd <salmanahmed1497@gmail.com>
2020-12-10 16:53:33 +0530

    simple hook

---
+++ src/component/counter/__test__/counter.test.js
@@ -0,0 +1,18 @@
+import Counter from '../';
+
+import { mount } from 'enzyme';
+
+
+// incorrect function assignment in the onClick method
+// will still pass the tests.
+
+test('the increment method increments count', () => {
+  const wrapper = mount(<Counter />)
+
+  expect(wrapper.instance().state.count).toBe(0)
+
+  // wrapper.find('button.counter-button').simulate('click')
+  // wrapper.setState({count: 1})
+  wrapper.instance().increment()
+  expect(wrapper.instance().state.count).toBe(1)
+})
